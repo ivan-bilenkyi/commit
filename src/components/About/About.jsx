426175de06705f6a0f8bfcbc3bb1546053f560b9
ivan-bilenkyi <ivanbilenkyi@gmail.com>
2024-09-04 20:05:00 +0300

    create about section

---
+++ src/components/About/About.jsx
@@ -0,0 +1,41 @@
+import css from './About.module.css'
+import image from '../../assets/images/builder.png'
+
+export const About = () => {
+  return (
+    <section className={css.section}>
+      <div className={css.top_wrapper}>
+        <p className={css.subtitle}>Про нас</p>
+        <h2 className={css.title}>Індивідуальний підхід та гнучкість для створення цифрових рішень</h2>
+        <p className={css.subtext}>Наша команда працює над тим, щоб кожне рішення було максимально адаптивним, інтуїтивним і орієнтованим на вашого кінцевого користувача.</p>
+      </div>
+      <div className={css.wrapper}>
+        <div className={css.image_wrapper}>
+          <img className={css.image} src={image} alt='майстер на червоному фоні'/>
+        </div>
+        <ul className={css.list}>
+          <li className={css.item_image_wrapper}>
+            <img className={css.item_image} src={image} alt="руки" />
+          </li>
+          <li className={`${css.item} ${css.item1}`}>
+            <span>1250+</span>
+            <p>виконаних проектів</p>
+          </li>
+          <li className={`${css.item} ${css.item2}`}>
+            <span>10mln+</span>
+            <p>рядків коду</p>
+          </li>
+          <li className={`${css.item} ${css.item3}`}>
+            <span>99%</span>
+            <p>задоволених клієнтів</p>
+          </li>
+          <li className={`${css.item} ${css.item4}`}>
+            <span>85%</span>
+            <p>зростання бізнесу</p>
+          </li>
+        </ul>
+
+      </div>
+    </section>
+  )
+}
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import { Hero } from '../../components/Hero/Hero.jsx';
 import { Header } from '../../components/Header/Header.jsx';
+import { About } from '../../components/About/About.jsx';
 
 function HomePage() {
   return (
     <>
       <Header/>
       <Hero/>
+      <About/>
     </>
   );
 }
