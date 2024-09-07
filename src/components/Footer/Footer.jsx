3b901a0dad4f51361583536b3c44fe1e01026cc1
ivan-bilenkyi <ivanbilenkyi@gmail.com>
2024-09-07 14:47:45 +0300

    update styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,11 +5,11 @@ export const Footer = () => {
   return (
     <section className={css.footer} id="footer">
       <div className={css.info_wrapper}>
-        <div>
+        <div className={css.wrapper}>
           <h2 className={css.footer_title}>COMMIT</h2>
           <h3 className={css.subtitle}>Комітимо мрії в реальність</h3>
         </div>
-        <div>
+        <div className={css.wrapper}>
           <h3 className={css.top_title}>Адреса</h3>
           <p>
             Київ, Україна <br />
@@ -17,7 +17,7 @@ export const Footer = () => {
             154, офіс / блок 4-5
           </p>
         </div>
-        <div>
+        <div className={css.wrapper}>
           <h3 className={css.top_title}>Телефони</h3>
           <ul>
             <li>+38 098-001-39-39</li>
@@ -25,7 +25,7 @@ export const Footer = () => {
             <li>+38 066-001-39-39</li>
           </ul>
         </div>
-        <div>
+        <div className={css.wrapper}>
           <h3 className={css.top_title}>Контакти</h3>
           <ul>
             <li>Email <span>gmail</span></li>
